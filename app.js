18920d953c9cd056b963dedf4bda2eda5c98d1a7
Daniel Araújo <daniels_araujo@outlook.com.br>
2026-04-25 09:41:23 -0300

    Enquanto...

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 alert('Boas vindas ao jogo do número secreto!');
 let numeroSecreto = 5;
 console.log(numeroSecreto);
-let chute = prompt('Escolha um número entre 1 e 30');
+let chute;
 
-// se o chute for igual ao número secreto
-if (chute == numeroSecreto) {
-    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}.`);
-} else {
-    if (chute > numeroSecreto) {
-        alert(`O número secreto é menor que ${chute}`);
+// enquanto o chute não for igual ao número secreto
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 30');
+    // se o chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}.`);
     } else {
-        alert(`O número secreto é maior que ${chute}`);
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
     }
 }
