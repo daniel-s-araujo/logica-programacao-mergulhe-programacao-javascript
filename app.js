3f0d2585773f0119f57bb6f49935c06dad728fd2
Daniel Araújo <daniels_araujo@outlook.com.br>
2026-05-02 11:05:07 -0300

    Break

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ while (chute != numeroSecreto) {
     chute = prompt('Escolha um número entre 1 e 10');
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
-        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+        break;
     } else {
         if (chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`);
@@ -20,3 +20,10 @@ while (chute != numeroSecreto) {
         tentativas++;
     }
 }
+
+if (tentativas > 1) {
+    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+} else {
+    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`);
+}
+
