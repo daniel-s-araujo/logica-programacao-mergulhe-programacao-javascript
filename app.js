d5dde921aca40c3e9b0ee309bfbab095814802a3
Daniel Araújo <daniels_araujo@outlook.com.br>
2026-05-16 18:28:56 -0300

    Tentativas

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ alert('Boas vindas ao jogo do número secreto!');
 let numeroSecreto = 5;
 console.log(numeroSecreto);
 let chute;
+let tentativas = 1;
 
 // enquanto chute não for igual ao número secreto
 while (chute != numeroSecreto) {
@@ -9,12 +10,14 @@ while (chute != numeroSecreto) {
 
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
-        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
+        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
     } else {
         if (chute > numeroSecreto) {
             alert(`O número secreto é menor que o ${chute}`);
         } else {
             alert(`O número secreto é maior que o ${chute}`);
         }
+        // tentativas = tentativas + 1;
+        tentativas++;
     }
 }
