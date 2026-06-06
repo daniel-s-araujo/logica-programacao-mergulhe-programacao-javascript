087ddb9e7869cb9d3a1b2a9fdc478c6ed2153010
Daniel Araújo <daniels_araujo@outlook.com.br>
2026-06-06 07:54:23 -0300

    Operador ternário

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
-// enquanto o chute não for igual ao número secreto
 while (chute != numeroSecreto) {
     chute = prompt('Escolha um número entre 1 e 10');
     
-    // se o chute for igual ao número secreto
     if (chute == numeroSecreto) {
         break;
     } else {
@@ -17,14 +15,10 @@ while (chute != numeroSecreto) {
         } else {
             alert(`O número secreto é maior que ${chute}`);
         }
-        // tentativas = tentativas + 1;
+
         tentativas++;
     }
 }
 
-if (tentativas > 1) {
-    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas!`);
-} else {
-    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa!`);
-}
-
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}!`);
