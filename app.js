7033639fae7ef72a69aca77f72fd549b753a8339
Daniel Araújo <daniels_araujo@outlook.com.br>
2026-09-11 23:26:30 -0300

    Operador ternário

--- app.js
+++ app.js
@@ -23,8 +23,5 @@ while (chute != numeroSecreto) {
     }
 }
 
-if (tentativas > 1) {
-    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
-} else {
-    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`);
-}
+let palavraTentivas = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentivas}!`);
